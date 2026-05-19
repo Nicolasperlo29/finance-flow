858bea73f2255d76800f0e8c874c22b3bf4db3e5
Nicolasperlo29 <nicolasperlo29@gmail.com>
2026-05-19 01:02:12 -0300

    feat: add api gateway and eureka service discovery

--- Backend/Auth-Service/src/main/java/org/example/authservice/config/RabbitConfig.java
+++ Backend/Auth-Service/src/main/java/org/example/authservice/config/RabbitConfig.java
@@ -6,7 +6,8 @@ import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.core.TopicExchange;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.amqp.support.converter.JacksonJsonMessageConverter;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -44,6 +45,6 @@ public class RabbitConfig {
 
     @Bean
     public MessageConverter messageConverter() {
-        return new JacksonJsonMessageConverter();
+        return new Jackson2JsonMessageConverter();
     }
 }
